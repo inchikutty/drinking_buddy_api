49a6dd4410a0ab2fa286ac34453d00bfd89cba0c
inchikutty <inchikutty13@gmail.com>
2016-05-27 03:10:21 +0530

    allow CORS headers

--- public/index.php
+++ public/index.php
@@ -5,6 +5,7 @@
  * @package  Laravel
  * @author   Taylor Otwell <taylorotwell@gmail.com>
  */
+header('content-type: application/json; charset=utf-8');
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
 /*
